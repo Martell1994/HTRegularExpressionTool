0f6be0128ff4d863eb976926648936a822e14d98
Martell1994 <Martell1994@icloud.com>
2017-07-06 12:32:49 +0800

    创建项目

---
+++ HTRegularExpressionTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTRegularExpressionTool
+//
+//  Created by Huiting Mao on 2017/7/6.
+//  Copyright © 2017年 Martell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
